b4b15b0bcfd2ee44455e28f80e09dcb6d58b7b44
messyorganised <100069865+messyorganised@users.noreply.github.com>
2023-10-31 23:45:28 +1100

    Add files via upload

---
+++ TrendInstaller.ps1
@@ -0,0 +1,7 @@
+﻿$ScriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+write-host "Enter Company Identifier"
+$identifierpath = Read-Host
+
+start-process msiexec '/i \$Scriptpath\Software\WFBS-SVC_Agent_Installer IDENTIFIER=$identifierpath ABANDON=0 SILENTMODE=1  /L*v C:\Windows\Temp\WFBS_Debug\wofie_msi.log'
+
