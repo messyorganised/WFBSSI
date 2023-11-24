c7f90838a6c905d21f0410f296dfc034337b0d0d
messyorganised <abgmihby2@gmail.com>
2023-11-24 11:28:07 +1100

    2

--- TrendInstaller.ps1
+++ TrendInstaller.ps1
@@ -3,17 +3,11 @@ $TrendMSI = Join-Path -Path ([Environment]::GetFolderPath("Desktop")) -ChildPath
 $TrendIdentifier = 'TENANT ID'
 
 function AVInstall {
-    write-host "
-    Downloading Trend Antivirus
-    "
+    write-host "Downloading Trend Antivirus" -Seperator "`n"
     start-process $Scriptpath\Software\WFBS-SVC_Downloader.exe /SILENT -wait
 
-    Write-Host "
-    Installing Trend Antivirus
-    "
+    Write-Host "Installing Trend Antivirus" -Seperator "`n"
     Start-Process msiexec.exe -ArgumentList "/i $TrendMSI IDENTIFIER=$Trendidentifier SILENTMODE=1"
     
-    Write-Host "
-    SUCCESS!
-    "
+    Write-Host "SUCCESS!" -Seperator "`n"
 }
